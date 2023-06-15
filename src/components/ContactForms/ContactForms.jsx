f611447ca9439c9bc6a554465f058fd9c4c1c285
MolchanovSergii <molchanse@gmail.com>
2023-06-15 14:10:32 +0300

    refactoring contactForm

--- src/components/ContactForms/ContactForms.jsx
+++ src/components/ContactForms/ContactForms.jsx
@@ -1,66 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { StyledForm, StyledInput, StyledButton } from './ContactForms.styled';
 
-class ContactForms extends Component {
-  state = {
-    name: '',
-    number: '',
+const ContactForms = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  // const handleInputChangeName = event => {
+  //   setName(event.target.value);
+  // };
+
+  // const handleInputChangeNumber = event => {
+  //   setNumber(event.target.value);
+  // };
+  // для замены onChange на каждый стейт, можно использовать switchб не забыть
+  // переменные свитч - это строка!!!
+  const uniqueId = nanoid();
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        console.warn(`Тип поля name -${name} не обрабатывается`);
+    }
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
-  };
-
-  handleInputChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmitForm = event => {
+  const handleSubmitForm = event => {
     event.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  uniqueId1 = nanoid();
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
 
-  render() {
-    return (
-      <StyledForm onSubmit={this.handleSubmitForm}>
-        <label htmlFor={this.uniqueId}>
-          Name
-          <StyledInput
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleInputChange}
-            id={this.uniqueId}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label htmlFor={this.uniqueId}>
-          Number
-          <StyledInput
-            type="tel"
-            name="number"
-            value={this.state.number}
-            onChange={this.handleInputChange}
-            id={this.uniqueId}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <StyledButton type="submit">Add contact</StyledButton>
-      </StyledForm>
-    );
-  }
-}
+  return (
+    <StyledForm onSubmit={handleSubmitForm}>
+      <label htmlFor={uniqueId}>
+        Name
+        <StyledInput
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          id={uniqueId}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label htmlFor={uniqueId}>
+        Number
+        <StyledInput
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          id={uniqueId}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <StyledButton type="submit">Add contact</StyledButton>
+    </StyledForm>
+  );
+};
 
 export default ContactForms;
